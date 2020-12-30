3d61b2ec7fc47c164d0f87df6346e28dbb1ca8a1
Dsm0 <willrinkoff@gmail.com>
2020-12-29 22:01:01 -0600

    updated SuperDirt interface functions for latest version of tidal

--- init/tidalScripts/SuperDirtInterface.hs
+++ init/tidalScripts/SuperDirtInterface.hs
@@ -1,24 +1,24 @@
 import qualified Data.Map as Map_
 
-:{
-let changeFunc' stream list = sendFunc' list
-      where toEvent' ws we ps pe v = Event (Sound.Tidal.Context.Context []) (Just $ Sound.Tidal.Context.Arc ws we) (Sound.Tidal.Context.Arc ps pe) v
-              -- where [ws',we',ps',pe'] = map toRational [ws,we,ps,pe]
-            makeFakeMap list_ = Map_.fromList list_
-            makeFuncHelp :: [(String,Value)] -> ControlPattern
-            makeFuncHelp y = Pattern $ fakeEvent (makeFakeMap y:: ControlMap)
-              where fakeEvent a notARealArgument = [(toEvent' 0 1 0 1) a]
-            makeFunc :: [(String,Value)] -> [ControlPattern]
-            makeFunc x = [makeFuncHelp x]
-            sendFunc' = mapM_ (streamFirst stream) . makeFunc
-    changeFunc stream func newFunction = changeFunc' stream list
-      where list = [(func, VS (render newFunction))]
-    resetFunc stream func = changeFunc stream func (makeJSVar "")
-    makeDraw stream newFunction = changeFunc stream "draw" newFunction
-    makeSetup stream newFunction = changeFunc stream "setup" newFunction
-    makeLoad stream newFunction = changeFunc stream "load" newFunction
-      -- where list = [("scMessage",VS "loadSoundFiles"),("filePath",VS path)]
-:}
+-- :{
+-- let changeFunc' stream list = sendFunc' list
+--       where toEvent' ws we ps pe v = Event (Sound.Tidal.Context.Context []) (Just $ Sound.Tidal.Context.Arc ws we) (Sound.Tidal.Context.Arc ps pe) v
+--               -- where [ws',we',ps',pe'] = map toRational [ws,we,ps,pe]
+--             makeFakeMap list_ = Map_.fromList list_
+--             makeFuncHelp :: [(String,Value)] -> ControlPattern
+--             makeFuncHelp y = Pattern $ fakeEvent (makeFakeMap y:: ControlMap)
+--               where fakeEvent a notARealArgument = [(toEvent' 0 1 0 1) a]
+--             makeFunc :: [(String,Value)] -> [ControlPattern]
+--             makeFunc x = [makeFuncHelp x]
+--             sendFunc' = mapM_ (streamFirst stream) . makeFunc
+--     changeFunc stream func newFunction = changeFunc' stream list
+--       where list = [(func, VS (render newFunction))]
+--     resetFunc stream func = changeFunc stream func (makeJSVar "")
+--     makeDraw stream newFunction = changeFunc stream "draw" newFunction
+--     makeSetup stream newFunction = changeFunc stream "setup" newFunction
+--     makeLoad stream newFunction = changeFunc stream "load" newFunction
+--       -- where list = [("scMessage",VS "loadSoundFiles"),("filePath",VS path)]
+-- :}
 
 :{
 let changeFunc' stream list = sendFunc' list
@@ -31,24 +31,25 @@ let changeFunc' stream list = sendFunc' list
             makeFunc :: [(String,Value)] -> [ControlPattern]
             makeFunc x = [makeFuncHelp x]
             sendFunc' = mapM_ (streamFirst stream) . makeFunc
+    vs x = VS x (Just 0)
     loadSoundFiles' stream path = changeFunc' stream list
-      where list = [("scMessage",VS "loadSoundFiles"),("filePath",VS path)]
+      where list = [("scMessage",vs "loadSoundFiles" ),("filePath",vs path)]
     loadSynthDefs' stream path = changeFunc' stream list
-      where list = [("scMessage",VS "loadSynthDefs"),("filePath",VS path)]
+      where list = [("scMessage",vs "loadSynthDefs"),("filePath",vs path)]
     loadOnly' stream path = changeFunc' stream list
-      where list = [("scMessage",VS "loadOnly"),("filePath",VS path)]
+      where list = [("scMessage",vs "loadOnly"),("filePath",vs path)]
     loadSoundFileFolder' stream path = changeFunc' stream list
-      where list = [("scMessage",VS "loadSoundFileFolder"),("filePath",VS path)]
+      where list = [("scMessage",vs "loadSoundFileFolder"),("filePath",vs path)]
     loadSoundFile' stream path = changeFunc' stream list
-      where list = [("scMessage",VS "loadSoundFile"),("filePath",VS path)]
+      where list = [("scMessage",vs "loadSoundFile"),("filePath",vs path)]
     freeAllSoundFiles' stream = changeFunc' stream list
-      where list = [("scMessage",VS "freeAllSoundFiles")]
+      where list = [("scMessage",vs "freeAllSoundFiles")]
     freeSoundFiles' stream name = changeFunc' stream list
-      where list = [("scMessage",VS "freeSoundFiles"),("filePath",VS name)]
+      where list = [("scMessage",vs "freeSoundFiles"),("filePath",vs name)]
     postSampleInfo' stream = changeFunc' stream list
-      where list = [("scMessage",VS "postSampleInfo")]
+      where list = [("scMessage",vs "postSampleInfo")]
     initFreqSynthWindow' stream = changeFunc' stream list
-      where list = [("scMessage",VS "initFreqSynthWindow")]
+      where list = [("scMessage",vs "initFreqSynthWindow")]
 :}
 
 
