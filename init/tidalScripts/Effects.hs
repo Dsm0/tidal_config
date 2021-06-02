8c8841187f4e93d5f50bd407972ae3d3ac4b0433
Dsm0 <willrinkoff@gmail.com>
2021-06-02 17:55:43 -0500

    added bones of default effects def.s

---
+++ init/tidalScripts/Effects.hs
@@ -0,0 +1,35 @@
+-- for use with the effects bindings in vim-tidals vimrc
+
+
+-- effects can be over-written at any time
+-- by design
+:{
+let q__ = id
+    w__ = id
+    e__ = id
+    r__ = id
+    t__ = id
+    y__ = id
+    u__ = id
+    i__ = id
+    o__ = id
+    p__ = id
+    a__ = id
+    s__ = id
+    d__ = id
+    f__ = id
+    g__ = id
+    h__ = id
+    j__ = id
+    k__ = id
+    l__ = id
+    z__ = id
+    x__ = id
+    c__ = id
+    v__ = id
+    b__ = id
+    n__ = id
+    m__ = id
+:}
+
+putStrLn "loaded Effects.hs"
